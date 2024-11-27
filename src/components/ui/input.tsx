9898a4d2484ecbc39fc5e8078d6dcba2b735bd8e
Damian Michałowski <58490484+damianmichalowski@users.noreply.github.com>
2024-11-27 22:20:50 +0100

    fix hover inputs

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,7 +7,7 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
-    const radius = 100; // change this to increase the rdaius of the hover effect
+    const radius = 100;
     const [visible, setVisible] = React.useState(false);
 
     let mouseX = useMotionValue(0);
@@ -25,7 +25,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           background: useMotionTemplate`
         radial-gradient(
           ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
-          var(--pink-500),
+          var(--purple-500),
           transparent 80%
         )
       `,
@@ -38,7 +38,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            `flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+            `flex h-10 w-full border-none bg-white dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
           file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
           focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
            disabled:cursor-not-allowed disabled:opacity-50
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -6,7 +6,7 @@ const Textarea = React.forwardRef<
 	HTMLTextAreaElement,
 	React.ComponentProps<"textarea">
 >(({ className, ...props }, ref) => {
-	const radius = 100; // change this to increase the radius of the hover effect
+	const radius = 100;
 	const [visible, setVisible] = React.useState(false);
 
 	let mouseX = useMotionValue(0);
@@ -27,7 +27,7 @@ const Textarea = React.forwardRef<
             ${
 							visible ? radius + "px" : "0px"
 						} circle at ${mouseX}px ${mouseY}px,
-            var(--pink-500),
+            var(--purple-500),
             transparent 80%
           )
         `,
@@ -38,7 +38,7 @@ const Textarea = React.forwardRef<
 			className="p-[2px] rounded-lg transition duration-300 group/textarea">
 			<textarea
 				className={cn(
-					"flex min-h-[80px] w-full rounded-md border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input px-3 py-2 text-base placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/textarea:shadow-none transition duration-400 md:text-sm",
+					"flex min-h-[80px] w-full rounded-md border-none bg-white dark:bg-zinc-800 text-black dark:text-white shadow-input px-3 py-2 text-base placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/textarea:shadow-none transition duration-400 md:text-sm",
 					className
 				)}
 				ref={ref}
